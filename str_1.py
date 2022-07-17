df5ddb9b1b55d20f08ba2fc1c9f67b7f69f9cd40
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-17 19:28:38 +0300

    Add files via upload

---
+++ str_1.py
@@ -0,0 +1,12 @@
+
+def search_substr(str, substr):
+  if sybstr in str:
+    print('substring is in string')
+  else:
+    print('there is no substring in string')
+y = 'y'
+while y == 'y':
+  input_str = input('enter main string: ')
+  input_substr = input('enter substring: ')
+  search_substr(input_str, input_substr)
+  y = input('restart?(y/n): ')
