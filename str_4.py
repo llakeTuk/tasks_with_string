2bd9a0fcee4f22aa2b9926c33353a46dd1516e2f
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-08-01 16:15:12 +0300

    Add files via upload

---
+++ str_4.py
@@ -0,0 +1,18 @@
+def str_fence(str):
+  letter_counter = 0
+  output_str = ''
+  for letter in str:
+    if letter.isalpha():
+      if letter_counter % 2 == 0:
+        output_str += letter.upper()
+      else:
+        output_str += letter.lower()
+      letter_counter += 1
+    else:
+      output_str += letter
+  return output_str
+y = 'y'
+while y == 'y':
+  input_str = input('enter string: ')
+  print(str_fence(input_str))
+  y = input('restart?(y/n): ')
